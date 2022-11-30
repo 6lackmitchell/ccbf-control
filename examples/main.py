635413a9162e5a3e1a8cfb4915045d21b5286429
Mitchell Black <6lack.mitchell@gmail.com>
2022-11-30 09:15:46 -0500

    Updates poetry files

--- examples/main.py
+++ examples/main.py
@@ -1,10 +1,10 @@
-from src.core.simulate import simulate
+from core.simulate import simulate
 
 vehicle = "bicycle"
 level = "dynamic"
 situation = "warehouse"
 
 end_time = 40.0
-timestep = 0.001
+timestep = 0.01
 
 success = simulate(end_time, timestep, vehicle, level, situation)
